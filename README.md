67d828ef6d0171c8123b86f13ff5ecc0370bf819
Brian R Hanke <brianrhanke@outlook.com>
2026-07-20 14:20:50 -0400

    upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# BHGCGafferCameraGuides
-Gaffer plugin adding camera guide overlays.
+# BHGC Gaffer Camera Guides
+
+Gaffer plugin that adds a tool for displaying Action Safe, Title Safe and Rule of Thirds camera overlay guides. Custimizable horizontal and vertical divisions and line width and color.
+
+# Installation
+
+Download this repo, extract anywhere and point GAFFER_EXTENSION_PATHS to the root folder. That's it!
---
+++ startup/gui/BhgcCameraGuides.py
@@ -0,0 +1,210 @@
+##########################################################################
+#
+# Copyright 2026 Brian R Hanke
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##########################################################################
+
+import IECore
+import Gaffer
+import GafferUI
+import GafferSceneUI
+import imath
+import math
+import weakref
+
+class GuideGadget( GafferUI.Gadget ) :
+
+	def __init__( self, tool ) :
+
+		GafferUI.Gadget.__init__( self, "GuideGadget" )
+		self.__tool = weakref.ref( tool )
+
+	def renderLayer( self, layer, style, reason ) :
+
+		tool = self.__tool()
+		if tool is None or not tool["active"].getValue():
+			return
+
+		titleSafeEnabled = tool["titleSafe"].getValue()
+		actionSafeEnabled = tool["actionSafe"].getValue()
+		ruleOfThirdsEnabled = tool["ruleOfThirds"].getValue()
+		if not ( titleSafeEnabled or actionSafeEnabled or ruleOfThirdsEnabled ):
+			return
+
+		resolutionGate = tool.view().resolutionGate()
+		if resolutionGate.isEmpty() :
+			return
+
+		viewportGadget = self.ancestor( GafferUI.ViewportGadget )
+		with GafferUI.ViewportGadget.RasterScope( viewportGadget ) :
+
+			gateSize = imath.V2f( resolutionGate.max() - resolutionGate.min() )
+			lineWidth = tool["lineWidth"].getValue()
+			lineColor = tool["lineColor"].getValue()
+
+			if titleSafeEnabled:
+				titleSize = gateSize * imath.V2f( math.sqrt( 0.8 ) )
+				titleSize = ( gateSize - titleSize ) / 2
+				titleMin = resolutionGate.min() + titleSize
+				titleMax = resolutionGate.max() - titleSize
+				edges = [
+					( imath.V3f( titleMin.x, titleMin.y, 0 ), imath.V3f( titleMax.x, titleMin.y, 0 ) ),
+					( imath.V3f( titleMax.x, titleMin.y, 0 ), imath.V3f( titleMax.x, titleMax.y, 0 ) ),
+					( imath.V3f( titleMax.x, titleMax.y, 0 ), imath.V3f( titleMin.x, titleMax.y, 0 ) ),
+					( imath.V3f( titleMin.x, titleMax.y, 0 ), imath.V3f( titleMin.x, titleMin.y, 0 ) )
+				]
+				for p1, p2 in edges:
+					style.renderLine( IECore.LineSegment3f( p1, p2 ), lineWidth, lineColor )
+
+			if actionSafeEnabled:
+				actionSize = gateSize * imath.V2f( math.sqrt( 0.9 ) )
+				actionSize = ( gateSize - actionSize ) / 2
+				actionMin = resolutionGate.min() + actionSize
+				actionMax = resolutionGate.max() - actionSize
+				edges = [
+					( imath.V3f( actionMin.x, actionMin.y, 0 ), imath.V3f( actionMax.x, actionMin.y, 0 ) ),
+					( imath.V3f( actionMax.x, actionMin.y, 0 ), imath.V3f( actionMax.x, actionMax.y, 0 ) ),
+					( imath.V3f( actionMax.x, actionMax.y, 0 ), imath.V3f( actionMin.x, actionMax.y, 0 ) ),
+					( imath.V3f( actionMin.x, actionMax.y, 0 ), imath.V3f( actionMin.x, actionMin.y, 0 ) )
+				]
+				for p1, p2 in edges:
+					style.renderLine( IECore.LineSegment3f( p1, p2 ), lineWidth, lineColor )
+
+			if ruleOfThirdsEnabled:
+				divNumH = tool["divNumH"].getValue()
+				divNumV = tool["divNumV"].getValue()
+				divSize = imath.V2f( gateSize / imath.V2f( divNumH, divNumV ) )
+
+				for v in range(1, divNumV):
+					y = resolutionGate.min().y + ( divSize.y * v )
+					p1 = imath.V3f( resolutionGate.min().x, y, 0.0 )
+					p2 = imath.V3f( resolutionGate.max().x, y, 0.0 )
+					style.renderLine( IECore.LineSegment3f( p1, p2 ), lineWidth, lineColor )
+				for h in range(1, divNumH):
+					x = resolutionGate.min().x + ( divSize.x * h )
+					p1 = imath.V3f( x, resolutionGate.min().y, 0.0 )
+					p2 = imath.V3f( x, resolutionGate.max().y, 0.0 )
+					style.renderLine( IECore.LineSegment3f( p1, p2 ), lineWidth, lineColor )
+
+	def layerMask( self ) :
+
+		return self.Layer.Front
+
+	def renderBound( self ) :
+
+		b = imath.Box3f()
+		b.makeInfinite()
+		return b
+
+class CameraGuides( GafferUI.Tool ) :
+
+	def __init__( self, view, name = "CameraGuides" ) :
+
+		GafferUI.Tool.__init__( self, view, name )
+
+		self["actionSafe"] = Gaffer.BoolPlug( "actionSafe", defaultValue = False )
+		self["titleSafe"] = Gaffer.BoolPlug( "titleSafe", defaultValue = False )
+		self["ruleOfThirds"] = Gaffer.BoolPlug( "ruleOfThirds", defaultValue = False )
+		self["divNumH"] = Gaffer.IntPlug( "divNumH", defaultValue = 3 )
+		self["divNumV"] = Gaffer.IntPlug( "divNumV", defaultValue = 3 )
+		self["lineWidth"] = Gaffer.FloatPlug( "lineWidth", defaultValue = 1.5 )
+		self["lineColor"] = Gaffer.Color4fPlug( "lineColor", defaultValue = imath.Color4f( 0.0, 0.25, 0.0, 1.0 ) )
+
+		self.__guideGadget = GuideGadget( self )
+		view.viewportGadget().addChild( self.__guideGadget )
+
+IECore.registerRunTimeTyped( CameraGuides, typeName = "GafferUI::CameraGuides" )
+GafferUI.Tool.registerTool( "CameraGuides", GafferSceneUI.SceneView, CameraGuides )
+
+Gaffer.Metadata.registerNode(
+	CameraGuides,
+
+	"description", "Guide overlays for the currently active camera.",
+	"nodeToolbar:bottom:type", "GafferUI.StandardNodeToolbar.bottom",
+	"tool:exclusive", False,
+
+	"toolbarLayout:activator:ruleOfThirdsEnabled", lambda plug : plug["ruleOfThirds"].getValue() == True,
+
+	plugs = {
+
+		"actionSafe" : {
+
+			"description" : "Action safe guide.",
+			"toolbarLayout:section" : "Bottom",
+
+		},
+		"titleSafe" : {
+
+			"description" : "Title safe guide.",
+			"toolbarLayout:section" : "Bottom",
+
+		},
+		"ruleOfThirds" : {
+
+			"description" : "Rule of thirds guide.",
+			"label" : "Rule of Thirds",
+			"toolbarLayout:section" : "Bottom",
+
+		},
+		"divNumH" : {
+
+			"description" : "Custom horizontal divisions.",
+			"label" : "Horizontal Divisions",
+			"toolbarLayout:visibilityActivator" : "ruleOfThirdsEnabled",
+			"toolbarLayout:width" : 50,
+			"toolbarLayout:section" : "Bottom",
+
+		},
+		"divNumV" : {
+
+			"description" : "Custom vertical divisions.",
+			"label" : "Vertical Divisions",
+			"toolbarLayout:visibilityActivator" : "ruleOfThirdsEnabled",
+			"toolbarLayout:width" : 50,
+			"toolbarLayout:section" : "Bottom",
+
+		},
+		"lineWidth" : {
+
+			"description" : "Line width.",
+			"label" : "Width",
+			"toolbarLayout:width" : 50,
+			"toolbarLayout:section" : "Bottom",
+
+		},
+		"lineColor" : {
+
+			"description" : "Guide color.",
+			"label" : "Color",
+			"toolbarLayout:width" : 200,
+			"toolbarLayout:section" : "Bottom",
+
+		},
+
+	},
+)
